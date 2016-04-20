305ddb5a4dee0d2ee092d175a54f3e337889d975
IndieForger <indieforger@gmail.com>
2016-04-20 18:27:03 +0100

    defaults

---
+++ defaults.js
@@ -0,0 +1,42 @@
+// --------------------------------------------------------------------
+// Important!
+// Do not modify this file.
+// Check README.md for more details on how to configure this package.
+// --------------------------------------------------------------------
+"use strict";
+var fs = require('fs');
+var configJSONPath = 'config.json';
+var Hoek = require('hoek');
+var config = {
+	server: {
+		host: process.env.PORT || 'localhost',
+		port: process.env.HOST || 8080
+	},
+	database: {
+		host: process.env.MONGO_HOST || '127.0.0.1',
+		port: process.env.MONGO_PORT || 27017,
+		db: process.env.MONGO_DB || 'hapi-subscriber',
+		username: process.env.MONGO_HOST || '',
+		password: process.env.MONGO_HOST || ''
+	},
+	auth: {
+		token: {
+			secret: process.env.TOKEN_SECRET || 'secret_token',
+			algorithms: [ 'HS256' ] // only allow HS256 algorithm
+		}
+	},
+	topics: [{
+		code: 'DEF:TOC',
+		required: true,
+		info: 'Site Terms and Conditions'
+	}]
+};
+
+try {
+	let options = JSON.parse(fs.readFileSync(configJSONPath, 'utf8'));
+	config = Hoek.applyToDefaults(config, options);
+} catch (e) {
+	config = Hoek.applyToDefaults(config, {});
+}
+
+module.exports = config;
